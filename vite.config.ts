36274e96f771db2727a02da3a6b767c8476504b9
motwaniy <motwani.y@husky.neu.edu>
2026-06-16 09:57:01 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { createServer } from "./server";
 
+export default defineConfig({
+    base: "/manishgangwani/yogeshmotwani.com/"
+});
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
